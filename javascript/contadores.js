aff4f3f59f292889c6688cca7b77554cb84c4ab7
Gabriel Forny <gabriel.muniz@quantumequity.com.br>
2024-06-16 11:06:34 -0300

    Adicionado função de contadores

---
+++ javascript/contadores.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Verifica se o dispositivo é móvel
+    const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+    // Se for um dispositivo móvel, inicia a animação imediatamente
+    if (isMobile) {
+        animateNumbers();
+    } else {
+        // Se não for, configura a verificação do viewport para acionar a animação
+        function checkViewportAndAnimateNumbers() {
+            if (isElementInViewport(document.getElementById("numeracaoSection"))) {
+                animateNumbers();
+                window.removeEventListener("scroll", checkViewportAndAnimateNumbers);
+                window.removeEventListener("resize", checkViewportAndAnimateNumbers);
+            }
+        }
+
+        window.addEventListener("scroll", checkViewportAndAnimateNumbers);
+        window.addEventListener("resize", checkViewportAndAnimateNumbers);
+    }
+});
+
+function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function animateNumbers() {
+    const duration = 2000;
+    const endValues = {
+        teoria: 100,
+        teoria2: 100,
+        teoria3: 100,
+        teoria4: 100
+    };
+
+    Object.entries(endValues).forEach(([key, end]) => {
+        let start = 0;
+        const element = document.getElementById(key);
+
+        const interval = setInterval(() => {
+            element.textContent = start + " +";
+            start += Math.ceil(end / (duration / 100));
+            if (start >= end) {
+                element.textContent = end + " +";
+                clearInterval(interval);
+            }
+        }, 50);
+    });
+}
