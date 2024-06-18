316fddad63204a402319f06e8e53d01eb70e9433
Gabriel Forny <gabriel.muniz@quantumequity.com.br>
2024-06-18 00:29:28 -0300

    Adicionando variação dos rating

--- javascript/depoimentos.js
+++ javascript/depoimentos.js
@@ -13,35 +13,7 @@ const depoimentos = [
         "texto": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type.",
         "rating": 4
     },
-
-    {
-        "nome": "Marcia 222Almeida",
-        "curso": "Otimizando o time operacional de limpeza",
-        "foto": "assets/images/matheus-lina.png",
-        "texto": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type.",
-        "rating": 4
-    },
-    {
-        "nome": "32323Marcia Almeida",
-        "curso": "Otimizando o time operacional de limpeza",
-        "foto": "assets/images/marcia-almeida.png",
-        "texto": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type.",
-        "rating": 4
-    },
-    {
-        "nome": "Marcia 222Almeida",
-        "curso": "Otimizando o time operacional de limpeza",
-        "foto": "assets/images/matheus-lina.png",
-        "texto": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type.",
-        "rating": 4
-    },
-    {
-        "nome": "32323Marcia Almeida",
-        "curso": "Otimizando o time operacional de limpeza",
-        "foto": "assets/images/marcia-almeida.png",
-        "texto": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type.",
-        "rating": 4
-    },
+    // Adicione mais depoimentos aqui
 ];
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -50,17 +22,19 @@ document.addEventListener("DOMContentLoaded", function () {
     depoimentos.forEach(depoimento => {
         const card = document.createElement("div");
         card.className = "depoimento-card";
+        let ratingStars = '';
+        for (let i = 0; i < depoimento.rating; i++) {
+            ratingStars += '<img class="rating-star" src="assets/icons/icone-rating.svg" alt="icone-rating" width="23px" height="23px">';
+        }
         card.innerHTML = `
             <div class="col-1">
                 <img src="${depoimento.foto}" alt="${depoimento.nome}">
             </div>
             <div class="col-2">
                 <div class="topo">
-                    <img class="rating" src="assets/icons/icone-rating.svg" alt="icone-rating" width: 23px; height: 23px;"> 
-                    <img class="aspas" src="assets/icons/icon-aspas.svg" alt="icone-aspas" width: 48px; height: 36px;"> 
-                
+                    <div class="rating">${ratingStars}</div>
+                    <img class="aspas" src="assets/icons/icon-aspas.svg" alt="icone-aspas" width="48px" height="36px"> 
                 </div>
-
                 <div class="info">
                     <p class="texto">${depoimento.texto}</p>
                     <p class="curso"><span>Curso:</span> ${depoimento.curso}</p>
