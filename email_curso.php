9e98edf31f04330f4b0ee093b3c83ad3dbb96dfe
Gabriel Forny <gabriel.muniz@quantumequity.com.br>
2024-11-07 18:13:09 -0300

    Ajuste para enviar email e redirecionamento

--- email_curso.php
+++ email_curso.php
@@ -3,13 +3,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = $_POST['nome'];
     $email = $_POST['email'];
     $telefone = $_POST['telefone'];
+    $curso = $_POST['curso'];
 
     $mensagem = "Nome: $nome\n";
     $mensagem .= "Email: $email\n";
     $mensagem .= "Telefone: $telefone\n";
+    $mensagem .= "Curso: $curso\n";
 
     $destinatario = "contato@escoladelimpeza.com.br";
-    $assunto = "Formulário - {curso}";
+    $assunto = "Formulário - $curso";
 
     // Cabeçalhos adicionais
     $headers = "From: $email";
@@ -18,8 +20,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     mb_internal_encoding("UTF-8");
 
     // Enviar email com cabeçalhos
-    mb_send_mail($destinatario, $assunto, $mensagem, $headers);
-
-    exit();
+    if (mb_send_mail($destinatario, $assunto, $mensagem, $headers)) {
+        // Redirecionamento com base no curso
+        switch ($curso) {
+            case "Tratamento de Pisos - para diversos setores":
+                header("Location: https://pay.hotmart.com/D95587814R");
+                break;
+        }
+        exit();
+    } else {
+        echo "Erro ao enviar o formulário. Tente novamente mais tarde.";
+    }
 }
-?>
